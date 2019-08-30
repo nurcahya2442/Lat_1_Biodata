a09e7a911ba408228b71b33526c618e9c81b64f1
nurcahya2442 <nurcahya2443@gmail.com>
2019-08-30 18:53:12 +0700

    PWPB Biodata

---
+++ app/src/main/java/com/example/lat_1_biodata/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lat_1_biodata;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
